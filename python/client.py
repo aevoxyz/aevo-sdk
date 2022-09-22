0ebcb4731b654aabb999ce52f272d189d35b6326
kenchangh <guanhao3797@gmail.com>
2022-09-22 18:57:17 +0800

    print

--- python/client.py
+++ python/client.py
@@ -1,3 +1,4 @@
+import os
 import json
 import random
 import requests
@@ -6,9 +7,12 @@ import asyncio
 import websockets
 from eip712_structs import EIP712Struct, Address, Uint, Boolean, make_domain
 from eth_account import Account
+from dotenv import load_dotenv
 
+load_dotenv()
 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545")) # This URL doesn't actually do anything, we just need a web3 instance
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545")) # This URL doesn"t actually do anything, we just need a web3 instance
+
 
 class Order(EIP712Struct):
     maker = Address()
@@ -31,87 +35,140 @@ class Client:
     
     @property
     def connections(self):
-        # return (self.public_connection, self.private_connection)
-        return (self.public_connection,)
+        return (self.public_connection, self.private_connection)
     
     async def open_connection(self):
-        self.public_connection = await websockets.connect('wss://ws.aevo.xyz')
-        # self.private_connection = await websockets.connect('ws://ws-auth.aevo.xyz')
+        self.public_connection = await websockets.connect("wss://ws.aevo.xyz")
+        self.private_connection = await websockets.connect("ws://ws-auth.aevo.xyz")
     
     async def close_connection(self):
         await self.public_connection.close()
-        # await self.private_connection.close()
+        await self.private_connection.close()
     
     async def recv(self):
         return json.loads(await self.public_connection.recv())
     
-    async def ping(self):
+    async def ping_all(self):
         for connection in self.connections:
-            await connection.send(json.dumps({
-                'op': 'ping',
-            }))
+            payload = {
+                "op": "ping",
+            }
+            if connection is self.private_connection:
+                payload.update(self.auth_payload())
+            await connection.send(json.dumps(payload))
             msg = json.loads(await connection.recv())
-            if msg['status'] != 'ok':
-                raise Exception('Ping failed')
+            if msg["status"] != "ok":
+                raise Exception(f"Ping failed: {msg['error']}")
     
-    def format_message(self, op, channel, data):
-        return json.dumps({
-            'op': op,
-            'channel': channel,
-            'data': data
-        })
+    def format_message(self, op, data, channel=None, private=False):
+        payload = {
+            "op": op,
+            "data": data
+        }
+        if private:
+            payload.update(self.auth_payload())
+        if channel:
+            payload.update({"channel": channel})
+        return json.dumps(payload)
     
-    def get_markets(self, asset='ETH'):
-        req = requests.get(f'https://api.aevo.xyz/markets?asset={asset}')
+    def auth_payload(self):
+        return {
+            "auth": {
+                "acc": self.wallet_address,
+                "key": self.api_key,
+            }
+        }
+    
+    def get_markets(self, asset="ETH"):
+        req = requests.get(f"https://api.aevo.xyz/markets?asset={asset}", verify=True)
         data = req.json()
-        if type(data) is dict and data.get('error'):
-            raise Exception('Failed to get markets')
+        if type(data) is dict and data.get("error"):
+            raise Exception("Failed to get markets")
         return data
     
     async def subscribe_ticker(self, instrument_name):
-        await self.public_connection.send(self.format_message('subscribe', 'ticker', {'instrument': instrument_name}))
+        await self.public_connection.send(json.dumps({
+            "op": "subscribe",
+            "channel": "ticker",
+            "data": {"instrument": instrument_name}
+        }))
     
     async def subscribe_orderbook(self, instrument_name):
-        await self.public_connection.send(self.format_message('subscribe', 'orderbook', {'instrument': instrument_name}))
+        await self.public_connection.send(json.dumps({
+            "op": "subscribe",
+            "channel": "orderbook",
+            "data": {"instrument": instrument_name}
+        }))
 
     async def subscribe_trades(self, instrument_name):
-        await self.public_connection.send(self.format_message('subscribe', 'trade', {'instrument': instrument_name}))
+        await self.public_connection.send(json.dumps({
+            "op": "subscribe",
+            "channel": "trade",
+            "data": {"instrument": instrument_name}
+        }))
     
     async def subscribe_index(self, asset):
-        await self.public_connection.send(self.format_message('subscribe', 'index', {'asset': asset}))
+        await self.public_connection.send(json.dumps({
+            "op": "subscribe",
+            "channel": "index",
+            "data": {"asset": asset}
+        }))
+
+    async def create_order(self, instrument_id, is_buy, limit_price, quantity):
+        salt, signature = self.sign_order(instrument_id, is_buy, limit_price, quantity)
+        payload = {
+            "op": "create_order",
+            "data": {
+                "instrument_id": str(instrument_id),
+                "maker": self.wallet_address,
+                "is_buy": is_buy,
+                "amount": str(int(quantity*10**6)),
+                "price": str(int(limit_price*10**6)),
+                "salt": str(salt),
+                "signature": signature
+            }
+        }
+        payload.update(self.auth_payload())
+        print(json.dumps(payload))
+        await self.private_connection.send(json.dumps(payload))
+
     
     def sign_order(self, instrument_id, is_buy, limit_price, quantity):
         salt = random.randint(0, 10**10) # we just need a large enough number
         decimals = 10**6
 
         order_struct = Order(
-            maker=self.wallet_address, # The wallet's main address
+            maker=self.wallet_address, # The wallet"s main address
             isBuy=is_buy,
             limitPrice=int(limit_price * decimals),
             amount=int(quantity * decimals),
             salt=salt,
             instrument=instrument_id)
 
-        domain = make_domain(name='Ribbon Exchange', version='1', chainId=1)
+        domain = make_domain(name="Ribbon Exchange", version="1", chainId=1)
         signable_bytes = Web3.keccak(order_struct.signable_bytes(domain=domain))
-        return Account._sign_hash(signable_bytes, self.private_key).signature.hex()
-
+        return salt, Account._sign_hash(signable_bytes, self.private_key).signature.hex()
+    
 
 async def main():
-    client = Client('INSERT_PRIVATE_KEY_HERE', 'INSERT_WALLET_ADDRESS_HERE', 'INSERT_API_KEY_HERE')
+    client = Client(os.environ["SIGNING_KEY"], os.environ["ACCOUNT_ADDRESS"], os.environ["API_KEY"])
 
     await client.open_connection()
-    await client.ping()
+    await client.ping_all()
     instruments = client.get_markets()
 
-    await asyncio.gather(*[client.subscribe_orderbook(instrument['instrument_name']) for instrument in instruments])
-    await asyncio.gather(*[client.subscribe_ticker(instrument['instrument_name']) for instrument in instruments])
-    await asyncio.gather(*[client.subscribe_trades(instrument['instrument_name']) for instrument in instruments])
-    await client.subscribe_index(asset='ETH')
+    print(client.address)
+    print(instruments[0])
+    await client.create_order(instruments[0]['instrument_id'], True, 10, 10)
+
+    # await asyncio.gather(*[client.subscribe_orderbook(instrument["instrument_name"]) for instrument in instruments])
+    # await asyncio.gather(*[client.subscribe_ticker(instrument["instrument_name"]) for instrument in instruments])
+    # await asyncio.gather(*[client.subscribe_trades(instrument["instrument_name"]) for instrument in instruments])
+    # await client.subscribe_index(asset="ETH")
 
     try:
         while True:
-            print(await client.recv())
+            print(await client.private_connection.recv())
     finally:
         await client.close_connection()
 
